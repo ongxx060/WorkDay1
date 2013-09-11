6a9873d6674411dcda88fa7d38a345cfc7a22f8b
idsc3101 <idsc3101@gmail.com>
2013-09-11 15:24:09 -0500

    I made a mistake and didn't solve it :(

--- problem1/BMICalc/BMICalc/Form1.vb
+++ problem1/BMICalc/BMICalc/Form1.vb
@@ -3,6 +3,27 @@
 Public Class Form1
 
     Private Sub btnCalcBMI_Click(sender As System.Object, e As System.EventArgs) Handles btnCalcBMI.Click
+        Dim hSt As String
+        Dim wSt As String
+
+        hSt = txtHeight.Text
+        wSt = txtWeight.Text
+
+        Dim h As Double
+        Dim w As Double
+
+        h = CDb1(hSt)
+        w = CDbl(wSt)
+
+        Dim BMI As Double
+        BMI = (w * 703) / (h ^ 2)
+
+        TextBox1.Text = CStr(BMI)
 
     End Sub
+
+    Private Function CDb1(hSt As String) As Double
+        Throw New NotImplementedException
+    End Function
+
 End Class
